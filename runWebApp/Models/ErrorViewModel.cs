7166bb28692501d5ca3b0a43e39df0f2817767f1
mlopez <ml.3012@gmail.com>
2024-07-13 10:05:39 -0300

    Agregar archivos de proyecto.

---
+++ runWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace runWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
