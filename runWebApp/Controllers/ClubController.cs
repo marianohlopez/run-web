6446e59cae2e709124d29ab5dcfaa8ade0039f96
mlopez <ml.3012@gmail.com>
2024-07-31 16:54:10 -0300

    create-controllers

---
+++ runWebApp/Controllers/ClubController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace runWebApp.Controllers
+{
+    public class ClubController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
